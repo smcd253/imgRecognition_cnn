a287b5aa422e96805439493f47f37a2b25da6224
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2019-04-03 10:18:43 -0700

    journal to script

--- pytorchMNIST_cnn.py
+++ pytorchMNIST_cnn.py
@@ -1,53 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Training a Classifier
-=====================
-This is it. You have seen how to define neural networks, compute loss and make
-updates to the weights of the network.
-Now you might be thinking,
-What about data?
-----------------
-Generally, when you have to deal with image, text, audio or video data,
-you can use standard python packages that load data into a numpy array.
-Then you can convert this array into a ``torch.*Tensor``.
--  For images, packages such as Pillow, OpenCV are useful
--  For audio, packages such as scipy and librosa
--  For text, either raw Python or Cython based loading, or NLTK and
-   SpaCy are useful
-Specifically for vision, we have created a package called
-``torchvision``, that has data loaders for common datasets such as
-Imagenet, CIFAR10, MNIST, etc. and data transformers for images, viz.,
-``torchvision.datasets`` and ``torch.utils.data.DataLoader``.
-This provides a huge convenience and avoids writing boilerplate code.
-For this tutorial, we will use the CIFAR10 dataset.
-It has the classes: ‘airplane’, ‘automobile’, ‘bird’, ‘cat’, ‘deer’,
-‘dog’, ‘frog’, ‘horse’, ‘ship’, ‘truck’. The images in CIFAR-10 are of
-size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
-.. figure:: /_static/img/cifar10.png
-   :alt: cifar10
-   cifar10
-Training an image classifier
-----------------------------
-We will do the following steps in order:
-1. Load and normalizing the CIFAR10 training and test datasets using
-   ``torchvision``
-2. Define a Convolutional Neural Network
-3. Define a loss function
-4. Train the network on the training data
-5. Test the network on the test data
-1. Loading and normalizing CIFAR10
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-Using ``torchvision``, it’s extremely easy to load CIFAR10.
-"""
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
+
+
+# train on gpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+print("DEVICE: ")
+print(device)
+print()
 
 ########################################################################
 # The output of torchvision datasets are PILImage images of range [0, 1].
 # We transform them to Tensors of normalized range [-1, 1].
 
-batchSize = 1
+batchSize = 4
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -88,8 +59,7 @@ images, labels = dataiter.next()
 # show images
 imshow(torchvision.utils.make_grid(images))
 # print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
+print(' '.join('%5s' % classes[labels[j]] for j in range(batchSize)))
 
 ########################################################################
 # 2. Define a Convolutional Neural Network
@@ -107,14 +77,15 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 6, 5, stride = 1, padding = 0)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5, stride = 1, padding = 0)
-        self.fc1 = nn.Linear(256 * batchSize, 120)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120) # x shape is 16 * 4 * 4
         self.fc2 = nn.Linear(120, 80)
         self.fc3 = nn.Linear(80, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 256 * batchSize)
+        #print(x.shape)
+        x = x.view(-1, 16 * 4 * 4) # x shape is 16 * 4 * 4
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -122,6 +93,9 @@ class Net(nn.Module):
 
 
 net = Net()
+
+#transfer to GPU
+net.to(device)
 
 ########################################################################
 # 3. Define a Loss function and optimizer
@@ -149,15 +123,22 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 # ADDED BY TA
 # net.train() # sets model to training mode (if you have drouout and BN, Batch Nom)
 
-for epoch in range(2):  # loop over the dataset multiple times
+losses = []
+epoch_losses = []
+num_epochs = 1
+
+fig = plt.figure()
+ax1 = fig.add_subplot()
+
+for epoch in range(num_epochs):  # loop over the dataset multiple times
 
     running_loss = 0.0
     # iterate over data
     for i, data in enumerate(trainloader, 0):
         # get the inputs
-        inputs, labels = data
+#         inputs, labels = data
         # transfer to gpu
-        # inputs, labels = inputs.to(device), labels.to(device)
+        inputs, labels = inputs.to(device), labels.to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -173,11 +154,22 @@ for epoch in range(2):  # loop over the dataset multiple times
         if i % 2000 == 1999:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
+            losses.append(running_loss/2000)
             running_loss = 0.0
+    plt.plot(losses, label = 'epoch %d' % (epoch + 1)) 
+    losses.clear()
+#     print("epoch_losses[%d] = " % (epoch + 1))
+#     print(*(epoch_losses + epoch), sep = ", ")
+    
+# for i, epoch in enumerate(epoch_losses,0):
+#   plt.plot(epoch, label = '%d' % (i + 1))
+
+plt.legend(loc='upper left');
+plt.show()
 
 print('Finished Training')
 
-########################################################################
+#######################################################################
 # 5. Test the network on the test data
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
@@ -230,31 +222,3 @@ with torch.no_grad():
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
-
-########################################################################
-# That looks waaay better than chance, which is 10% accuracy (randomly picking
-# a class out of 10 classes).
-# Seems like the network learnt something.
-#
-# Hmmm, what are the classes that performed well, and the classes that did
-# not perform well:
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(4):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
-
-
