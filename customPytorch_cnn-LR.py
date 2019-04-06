c1c4f0f2ac063251a89f1716e267b4f81b22fc45
Spencer McDonough <SwaggySpencerMcDee@Spencers-Air-3.lan>
2019-04-06 14:13:57 -0700

    fix formatting attempt 2

--- customPytorch_cnn-LR.py
+++ customPytorch_cnn-LR.py
@@ -102,8 +102,8 @@ def plotLoss(lossesByEpoch, batchSize, rate):
     lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.xlabel("Mini-Batch Per Epoch (size 2000)")
     plt.ylabel("Loss")
-    title = fig.suptitle("Training Loss by Epoch (Batch Size = %d) (Learning Rate = %.3f)" % (batchSize, learning_rates[rate]))
-    fig.savefig('%s/trainLoss_by_epoch_BS_%d_LR_%.3f' % (writeLoc, batchSize, learning_rates[rate]), bbox_extra_artists=(lgd,title), bbox_inches='tight')
+    title = fig.suptitle("Training Loss by Epoch (Batch Size = %d) (Learning Rate = %.3f)" % (batchSize, rate))
+    fig.savefig('%s/trainLoss_by_epoch_BS_%d_LR_%.3f' % (writeLoc, batchSize, rate), bbox_extra_artists=(lgd,title), bbox_inches='tight')
 
 def plotAccuracy(trainAccByEpoch, batchSize, rate):
     fig = plt.figure(figsize = [8.4, 4.8])
@@ -118,8 +118,8 @@ def plotAccuracy(trainAccByEpoch, batchSize, rate):
     lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.xlabel("Mini-Batch Per Epoch (size 2000)")
     plt.ylabel("Accuracy (%)")
-    title = fig.suptitle("Training Accuracy by Epoch (Batch Size = %d) (Learning Rate = %.3f)" % (batchSize, learning_rates[rate]))
-    fig.savefig('%s/trainAcc_by_epoch_BS_%d_LR_%.3f' % (writeLoc, batchSize, learning_rates[rate]), bbox_extra_artists=(lgd,title), bbox_inches='tight')
+    title = fig.suptitle("Training Accuracy by Epoch (Batch Size = %d) (Learning Rate = %.3f)" % (batchSize, rate))
+    fig.savefig('%s/trainAcc_by_epoch_BS_%d_LR_%.3f' % (writeLoc, batchSize, rate), bbox_extra_artists=(lgd,title), bbox_inches='tight')
 
 
 # train on gpu
