52d4cddc86c90467df3e4760d69fc6fdfa553ac7
Spencer McDonough <SwaggySpencerMcDee@Spencers-Air-3.lan>
2019-04-06 12:36:16 -0700

    loop over learning rates, test high batch sizes

--- customPytorch_cnn-LR.py
+++ customPytorch_cnn-LR.py
@@ -9,11 +9,9 @@ from PIL import Image
 
 import sys
 
-
-
 arg = sys.argv[1]
 if arg is "negative" or arg is "neg" or arg is "n":
-    writeLoc = "negativeTorchResults"
+    writeLoc = "negativeTorchResults-LR"
     log = open("negativeTorchResults/log.txt", "w")
     print("Running MNIST Negatives Through CNN")
     log.writelines("Running MNIST Negatives Through CNN")
@@ -23,7 +21,7 @@ if arg is "negative" or arg is "neg" or arg is "n":
                 transforms.Normalize((0.5,), (1.0,))]
                 )
 elif arg is "standard" or arg is "std" or arg is "s":
-    writeLoc = "torchResults"
+    writeLoc = "torchResults-LR"
     log = open("torchResults/log.txt", "w")
     print("Running Standard MNIST Through CNN")
     log.writelines("Running Standard MNIST Through CNN")
@@ -33,15 +31,15 @@ elif arg is "standard" or arg is "std" or arg is "s":
 
 exec = sys.argv[2]
 if exec is "debug" or exec is "d":
+    learning_rates = [0.001]
     batch_sizes = [1, 2]
     num_epochs = 4
-elif exec is "debugHigh" or exec is "dH":
-    batch_sizes = [256, 1024]
-    num_epochs = 4
 elif exec is "test" or exec is "t":
+    learning_rates = [0.001, 0.01, 0.1]
     batch_sizes = [1, 2, 4, 8, 16, 32]
     num_epochs = 16
 elif exec is "high" or exec is "h":
+    learning_rates = [0.001, 0.01, 0.1]
     batch_sizes = [64, 128, 256, 1024]
     num_epochs = 16
 
@@ -75,7 +73,23 @@ def plotBatchAccuracy(epoch_accuracy, batchSize): # source: https://stackoverflo
     fig.suptitle("Test Accuracy per Epoch (Batch Size = %d)" % (batchSize))
     fig.savefig("%s/testAcc_batchSize_%d.png" % (writeLoc, batchSize))
 
-def plotLoss(lossesByEpoch, batchSize):
+def plotBatchRateAccuracy(epochAcc_byRate, batchSize):
+    fig = plt.figure(figsize = [7.4, 4.8])
+    axs = fig.add_subplot(111)
+    for a in epochAcc_byRate:
+        a.insert(0,0)
+    xint = range(1, epochAcc_byRate[0].__len__())
+    plt.xticks(xint)
+    for rateNum, epoch_accuracies in enumerate(epochAcc_byRate):
+        axs.plot(epoch_accuracies, label = 'rate %d' % (learning_rates[rateNum] + 1))
+    plt.xlim(left = 1)
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    fig.suptitle("Test Accuracy per Epoch Over Different Rates (Batch Size = %d)" % (batchSize))
+    fig.savefig("%s/testAcc_batchSize_%d.png" % (writeLoc, batchSize))
+
+def plotLoss(lossesByEpoch, batchSize, rate):
     fig = plt.figure(figsize = [7.4, 4.8])
     axs = fig.add_subplot(111)
     for l in lossesByEpoch:
@@ -88,10 +102,10 @@ def plotLoss(lossesByEpoch, batchSize):
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.xlabel("Mini-Batch Per Epoch (size 2000)")
     plt.ylabel("Loss")
-    fig.suptitle("Training Loss by Epoch (Batch Size = %d)" % (batchSize))
-    fig.savefig('%s/trainLoss_by_epoch_batchSize_%d' % (writeLoc, batchSize))
+    fig.suptitle("Training Loss by Epoch (Batch Size = %d) (Learning Rate = %d)" % (batchSize, rate))
+    fig.savefig('%s/trainLoss_by_epoch_BS_%d_LR_%d' % (writeLoc, batchSize, rate))
 
-def plotAccuracy(trainAccByEpoch, batchSize):
+def plotAccuracy(trainAccByEpoch, batchSize, rate):
     fig = plt.figure(figsize = [7.4, 4.8])
     axs = fig.add_subplot(111)
     for a in trainAccByEpoch:
@@ -104,8 +118,8 @@ def plotAccuracy(trainAccByEpoch, batchSize):
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.xlabel("Mini-Batch Per Epoch (size 2000)")
     plt.ylabel("Accuracy (%)")
-    fig.suptitle("Training Accuracy by Epoch (Batch Size = %d)" % (batchSize))
-    fig.savefig('%s/trainAcc_by_epoch_batchSize_%d' % (writeLoc, batchSize))
+    fig.suptitle("Training Accuracy by Epoch (Batch Size = %d) (Learning Rate = %d)" % (batchSize, rate))
+    fig.savefig('%s/trainAcc_by_epoch_BS_%d_LR_%d' % (writeLoc, batchSize, rate))
 
 
 # train on gpu
@@ -183,9 +197,9 @@ for b, batchSize in enumerate(batch_sizes):
             x = self.pool(F.relu(self.conv2(x)))
             #print(x.shape)
             x = x.view(-1, 16 * 4 * 4) # x shape is 16 * 4 * 4
-            x = F.relu(self.fc1(x)) # relu activation function on 
+            x = F.relu(self.fc1(x)) # relu activation function
             x = F.relu(self.fc2(x))
-            x = F.softmax(self.fc3(x))
+            x = F.softmax(self.fc3(x)) # softmax activation function
             return x
 
 
@@ -202,96 +216,103 @@ for b, batchSize in enumerate(batch_sizes):
     import torch.optim as optim
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    
+    epochAcc_byRate = []
+    # loop over different learning rates to see effect on accuracy
+    for r, rate in enumerate(learning_rates):
+        optimizer = optim.SGD(net.parameters(), lr=rate, momentum=0.9)
 
-    ########################################################################
-    # 4. Train the network
-    # ^^^^^^^^^^^^^^^^^^^^
-    #
-    # This is when things start to get interesting.
-    # We simply have to loop over our data iterator, and feed the inputs to the
-    # network and optimize.
-
-    net.train() # sets model to training mode (if you have drouout and BN, Batch Nom)
-
-    lossesByEpoch = []
-    trainAccByEpoch = []
-    epoch_accuracy = deque([]) # list to hold accuracy of test over epochs
-
-    for epoch in range(num_epochs):  # loop over the dataset multiple times
-        losses = []
-        accuracies = []
-        running_loss = 0.0
-        correct_t, total_t = 0, 0
-        # iterate over data
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs
-            inputs, labels = data
-            # transfer to gpu
-            inputs, labels = inputs.to(device), labels.to(device)
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward() # back-propogate loss
-            optimizer.step() # update weight
-
-
-            # calculate accuracy of predictions in the current batch
-            _t, predicted_t = torch.max(outputs.data, 1)
-            total_t += labels.size(0)
-            correct_t += (predicted_t == labels).sum().item()
-            train_acc = 100. * correct_t/total_t
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('Epoch %d/%d | Mini-Batch %5d | Loss: %.3f | Accuracy: %.3f' % (epoch + 1, num_epochs, i + 1, running_loss / 2000, train_acc))
-                log.writelines('Epoch %d/%d | Mini-Batch %5d | Loss: %.3f | Accuracy: %.3f\n' % (epoch + 1, num_epochs, i + 1, running_loss / 2000, train_acc))
-
-                losses.append(running_loss/2000)
-                running_loss = 0.0
-                #Accuracy
-                accuracies.append(train_acc)
-
-        lossesByEpoch.append(losses)
-        trainAccByEpoch.append(accuracies)
-
-        #######################################################################
-        # 5. Test the network on the test data
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        ########################################################################
+        # 4. Train the network
+        # ^^^^^^^^^^^^^^^^^^^^
         #
-        # We will check this by predicting the class label that the neural network
-        # outputs, and checking it against the ground-truth. If the prediction is
-        # correct, we add the sample to the list of correct predictions.
+        # This is when things start to get interesting.
+        # We simply have to loop over our data iterator, and feed the inputs to the
+        # network and optimize.
 
-        correct = 0
-        total = 0
-        net.eval()
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data
-                images, labels = images.to(device), labels.to(device)        
-                outputs = net(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        accuracy = 100 * float(correct / total)
-        print('Accuracy of the network on the 10000 test images after epoch %d: %f %%' % (epoch + 1, accuracy))
-        log.writelines('Accuracy of the network on the 10000 test images after epoch %d: %f %%\n' % (epoch + 1, accuracy))
-        epoch_accuracy.append(accuracy)
+        net.train() # sets model to training mode (if you have drouout and BN, Batch Nom)
 
-    # plot losses
-    plotLoss(lossesByEpoch, batchSize)
+        lossesByEpoch = []
+        trainAccByEpoch = []
+        epoch_accuracy = [] # list to hold accuracy of test over epochs
 
-    # plot accuracy (100 - loss)
-    plotAccuracy(trainAccByEpoch, batchSize)
+        for epoch in range(num_epochs):  # loop over the dataset multiple times
+            losses = []
+            accuracies = []
+            running_loss = 0.0
+            correct_t, total_t = 0, 0
+            # iterate over data
+            for i, data in enumerate(trainloader, 0):
+                # get the inputs
+                inputs, labels = data
+                # transfer to gpu
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward() # back-propogate loss
+                optimizer.step() # update weight
+
+
+                # calculate accuracy of predictions in the current batch
+                _t, predicted_t = torch.max(outputs.data, 1)
+                total_t += labels.size(0)
+                correct_t += (predicted_t == labels).sum().item()
+                train_acc = 100. * correct_t/total_t
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    print('Epoch %d/%d | Mini-Batch %5d | Loss: %.3f | Accuracy: %.3f' % (epoch + 1, num_epochs, i + 1, running_loss / 2000, train_acc))
+                    log.writelines('Epoch %d/%d | Mini-Batch %5d | Loss: %.3f | Accuracy: %.3f\n' % (epoch + 1, num_epochs, i + 1, running_loss / 2000, train_acc))
+
+                    losses.append(running_loss/2000)
+                    running_loss = 0.0
+                    #Accuracy
+                    accuracies.append(train_acc)
+
+            lossesByEpoch.append(losses)
+            trainAccByEpoch.append(accuracies)
+
+            #######################################################################
+            # 5. Test the network on the test data
+            # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            #
+            # We will check this by predicting the class label that the neural network
+            # outputs, and checking it against the ground-truth. If the prediction is
+            # correct, we add the sample to the list of correct predictions.
+
+            correct = 0
+            total = 0
+            net.eval()
+            with torch.no_grad():
+                for data in testloader:
+                    images, labels = data
+                    images, labels = images.to(device), labels.to(device)        
+                    outputs = net(images)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+            accuracy = 100 * float(correct / total)
+            print('Accuracy of the network on the 10000 test images after epoch %d: %f %%' % (epoch + 1, accuracy))
+            log.writelines('Accuracy of the network on the 10000 test images after epoch %d: %f %%\n' % (epoch + 1, accuracy))
+            epoch_accuracy.append(accuracy)
+        
+        # append epoch_accuracy to plot by rate
+        epochAcc_byRate.append(epoch_accuracy)
+        
+        # plot losses
+        plotLoss(lossesByEpoch, batchSize, rate)
+
+        # plot accuracy (100 - loss)
+        plotAccuracy(trainAccByEpoch, batchSize, rate)
     
     # plot accuracy
-    plotBatchAccuracy(epoch_accuracy, batchSize)
+    plotBatchRateAccuracy(epochAcc_byRate, batchSize)
 
 log.close()    
     
